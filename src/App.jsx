7d60af24418cf777c8bc9a6739aba9394673bd61
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-04-14 00:15:37 +0200

    feat: menu selezione genere

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,20 @@ function App() {
   return (
     <>
       <h1>Film disponibili:</h1>
+
       <ul>
         {dataMovies.map((movie, index) => <li key={index}>
           {movie.title} {'->'} {movie.genre}
         </li>)}
       </ul>
+
+      <select>
+        <option value="">Nessuno</option>
+        <option value="Fantascienza">Fantascienza</option>
+        <option value="Thriller">Thriller</option>
+        <option value="Romantico">Romantico</option>
+        <option value="Azione">Azione</option>
+      </select>
     </>
   )
 }
